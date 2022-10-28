c50446b90a603cd9074a1430d40049f9dc416f93
LahcenHaouch <lahcen.haouch@protonmail.com>
2022-10-28 22:17:44 +0200

    Add delete button to each lead

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-const leads = JSON.parse(localStorage.getItem('leads')) ?? []
+// Fetch leads from localStorage or initialize it to an empty array
+let leads = JSON.parse(localStorage.getItem('leads')) ?? []
 
 const addLeadInput = document.querySelector('#add-lead-input')
 const addLeadBtn = document.querySelector('#add-lead-btn')
 const leadList = document.querySelector('#lead-list')
 
-function resetInput(input) {
-  input.value = ''
+function renderList(leads) {
+  console.log({leads})
+
+  leadList.innerHTML = leads.map((lead, index) => (`<li>${lead} <button type="button" onclick="deleteLead(${index})">X</button></li>`)).join('')
 }
 
-function renderList(listElement, leads) {
-  listElement.innerHTML = leads.map(lead => (`<li>${lead}</li>`)).join('')
+function saveInLocalStorage(elements) {
+  localStorage.setItem('leads', JSON.stringify(elements))
 }
 
-function addLead(input, leadList, leads) {
+function deleteLead(leadIndex) {
+  if (leadIndex === 0) {
+    leads = leads.slice(1)
+  } else {
+    console.log('first slice', leads.slice(0, leadIndex - 1))
+    console.log('second slice', leads.slice(leadIndex + 1))
+
+    leads = [...leads.slice(0, leadIndex), ...leads.slice(leadIndex + 1)]
+  }
+  
+  saveInLocalStorage(leads)
+  renderList(leads)
+}
+
+function addLead(input) {
   if (!input.value) return
 
   leads.push(input.value)
-  localStorage.setItem('leads', JSON.stringify(leads))
+  // Update localStorage on each added lead
+  saveInLocalStorage(leads)
 
-  resetInput(input)
-  renderList(leadList, leads)
+  input.value = ''
+
+  renderList(leads)
 }
 
 document.addEventListener('keydown', ({ key }) => {
   if (key === 'Enter') {
-    addLead(addLeadInput, leadList, leads)
+    addLead(addLeadInput)
   }
 })
 
 addLeadBtn.addEventListener('click', () => {
-  addLead(addLeadInput, leadList, leads)
+  addLead(addLeadInput)
 })
 
-renderList(leadList, leads)
+renderList(leads)
